ee35f9ae3009242a66565ef145c9495f974f5a62
Jeff Iacono <jeff.iacono@gmail.com>
2021-12-24 16:41:08 -0800

    Adds script to revoke admin ERC-20 privilages
    ```
    $ node scripts/11-revoke-roles.js
    👋 Your add address is: 0xA7a20Be51361BF4ee78b00556cbc8e16E1960d31
    👀 Roles that exist right now: {
      admin: [ '0x98D38Edd1121FBB63eD3f9b186ae04c3B4CB8966' ],
      minter: [
        '0x98D38Edd1121FBB63eD3f9b186ae04c3B4CB8966',
        '0xa759E185aca858C15F3899B593DeD46904502310'
      ],
      pauser: [ '0x98D38Edd1121FBB63eD3f9b186ae04c3B4CB8966' ],
      transfer: [ '0x98D38Edd1121FBB63eD3f9b186ae04c3B4CB8966' ]
    }
    🎉 Roles after revoking ourselves {
      admin: [],
      minter: [ '0xa759E185aca858C15F3899B593DeD46904502310' ],
      pauser: [],
      transfer: []
    }
    ✅ Successfully revoked our superpowers from the ERC-20 contract
    ```

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,27 @@
+import sdk from "./1-initialize-sdk.js";
+
+const tokenModule = sdk.getTokenModule(
+  "0x6d24e04915a2e24856656a511edd7c7604f3fd85"
+);
+
+(async () => {
+  try {
+    // log the current roles
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log(
+      "✅ Successfully revoked our superpowers from the ERC-20 contract"
+    );
+  } catch (error) {
+    console.error("🔴 Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
